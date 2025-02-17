6272117d33e665270c581f5b323d9791a9c303da
tuutoo <tuutoo@gmail.com>
2025-02-17 21:09:33 +0800

    feat: update version only

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,7 +7,7 @@ const { LL } = typesafeI18n()
 <template>
   <nav class="w-full flex items-center justify-between px-4 py-2">
     <div class="flex-1 flex justify-center">
-      <span text-3xl>{{ LL.title() }} v3.0.2</span>
+      <span text-3xl>{{ LL.title() }} v3.0.3</span>
     </div>
     <div class="flex items-center gap-2">
       <LanguageSelector />
